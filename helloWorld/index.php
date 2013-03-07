52d73edc996116e0a8f183e600f9c54996b5a5dd
odaepo <odaepo@gmail.com>
2013-03-07 16:14:13 +0100

    example in http://dojotoolkit.org/documentation/tutorials/1.8/hello_dojo/

--- helloWorld/index.php
+++ helloWorld/index.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Tutorial: Hello Dojo!</title>
+</head>
+<body>
+<h1 id="greeting">Hello</h1>
 
-echo "ciao";
+
+<!-- load Dojo -->
+<script src="//ajax.googleapis.com/ajax/libs/dojo/1.8.3/dojo/dojo.js"
+        data-dojo-config="async: true"></script>
+
+
+
+
+
+
+
+</body>
+</html>
